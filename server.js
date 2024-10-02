f43880dcfd5fef7cfa2002d93621bd646ef16549
RosarioManny <rosario.emm47@gmail.com>
2024-10-02 10:30:05 -0400

    Added custom middleware

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
 const recipesController = require('./controllers/recipes.js');
@@ -30,8 +32,9 @@ app.use(
     saveUninitialized: true,
   })
 );
-
+app.use(passUserToView);
 app.use('/auth', authController);
+app.use(isSignedIn);
 app.use('/recipes', recipesController);
 app.use('/ingredients', ingredientsController);
 
